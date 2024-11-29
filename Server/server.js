2462ded4d9f1f4cb280ad959ad153c75a6eef0f8
dr14nium <adrianhokas.work@gmail.com>
2024-11-30 06:37:52 +0700

    test server

--- Server/server.js
+++ Server/server.js
@@ -8,23 +8,30 @@ const { Pool } = pkg;
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Middleware
 app.use(cors());
-app.use(bodyParser.json()); // JSON parser
-app.use(bodyParser.urlencoded({ extended: false })); // URL-encoded parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// PostgreSQL connection configuration local server
+/*// PostgreSQL Welcome
+app.get('/', (req, res) => {
+  res.send('Welcome to the Terrasync Server API!');
+});
+*/
+
+// PostgreSQL connection configuration
 const pool = new Pool({
-  user: 'postgres',
-  host: '34.34.218.166',
-  database: 'data',
-  password: 'postgres',
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || '34.34.218.166', // Host IP
+  database: process.env.DB_NAME || 'data',
+  password: process.env.DB_PASSWORD || 'postgres',
   port: 5432,
 });
 
-// Check connection to PostgreSQL once on start
+// Test connection on startup
 pool.connect((err, client, release) => {
   if (err) {
-    console.error('Error acquiring client', err.stack);
+    console.error('Error connecting to PostgreSQL:', err.stack);
     return;
   }
   console.log('Connected to PostgreSQL');
@@ -36,71 +43,60 @@ app.get('/geojson/:tableName/:id?', async (req, res) => {
   const { tableName, id } = req.params;
 
   // Validate tableName
-  if (!/^[\w]+$/.test(tableName)) {
+  if (!/^[a-zA-Z0-9_]+$/.test(tableName)) {
     return res.status(400).json({ error: 'Invalid table name' });
   }
 
   try {
     const sanitizedTableName = pgFormat.ident(tableName); // Sanitize table name
-    const idCondition = id ? `WHERE t.id = $1` : '';
+    let query, params;
 
     if (id) {
-      // Query for single feature
-      const querySingleFeature = `
-          SELECT jsonb_build_object(
-            'type', 'Feature',
-            'geometry', ST_AsGeoJSON(t.geom, 15)::jsonb,
-            'properties', to_jsonb(t) - 'geom'
-          ) AS geojson
-          FROM public.${sanitizedTableName} t
-          WHERE t.id = $1;
-        `;
-
-      const result = await pool.query(querySingleFeature, [id]);
-
-      if (result.rows.length === 0) {
-        return res.status(404).json({ error: 'Data not found' });
-      }
-
-      res.setHeader('Content-Type', 'application/json');
-      return res.json(result.rows[0].geojson);
+      query = `
+        SELECT jsonb_build_object(
+          'type', 'Feature',
+          'geometry', ST_AsGeoJSON(t.geom, 15)::jsonb,
+          'properties', to_jsonb(t) - 'geom'
+        ) AS geojson
+        FROM public.${sanitizedTableName} t
+        WHERE t.id = $1;
+      `;
+      params = [id];
     } else {
-      // Query for all features
-      const queryAllFeatures = `
-          SELECT jsonb_build_object(
-            'type', 'FeatureCollection',
-            'features', jsonb_agg(
-              jsonb_build_object(
-                'type', 'Feature',
-                'geometry', ST_AsGeoJSON(t.geom, 15)::jsonb,
-                'properties', to_jsonb(t) - 'geom'
-              )
+      query = `
+        SELECT jsonb_build_object(
+          'type', 'FeatureCollection',
+          'features', jsonb_agg(
+            jsonb_build_object(
+              'type', 'Feature',
+              'geometry', ST_AsGeoJSON(t.geom, 15)::jsonb,
+              'properties', to_jsonb(t) - 'geom'
             )
-          ) AS geojson
-          FROM public.${sanitizedTableName} t;
-        `;
-
-      const result = await pool.query(queryAllFeatures);
-
-      if (result.rows.length === 0 || !result.rows[0].geojson.features) {
-        return res.status(204).send();
-      }
-
-      res.setHeader('Content-Type', 'application/json');
-      return res.json(result.rows[0].geojson);
+          )
+        ) AS geojson
+        FROM public.${sanitizedTableName} t;
+      `;
+      params = [];
     }
+
+    const result = await pool.query(query, params);
+
+    if (!result.rows.length || !result.rows[0].geojson) {
+      return res.status(404).json({ error: 'Data not found' });
+    }
+
+    res.json(result.rows[0].geojson);
   } catch (error) {
     console.error('Error fetching data:', error.stack);
     res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
 });
 
-// Endpoint 2: Query for PlaceAutoComplete
+// Endpoint 2: Search Place AutoComplete
 app.get('/search', async (req, res) => {
   const { query } = req.query;
 
   if (!query) {
-    console.log('Query parameter is missing');
     return res.status(400).json({ error: 'Query parameter is required' });
   }
 
@@ -124,81 +120,20 @@ app.get('/search', async (req, res) => {
       LIMIT 10;
     `;
 
-    console.log('Executing SQL Query:', sqlQuery, 'with parameter:', `%${query}%`);
     const result = await pool.query(sqlQuery, [`%${query}%`]);
 
-    console.log('Query Result:', result.rows);
-
-    if (result.rows.length === 0) {
-      console.log('No results found for query:', query);
+    if (!result.rows.length) {
       return res.status(404).json({ error: 'No results found' });
     }
 
     res.json(result.rows);
   } catch (error) {
-    console.error('Error fetching search results:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-// Endpoint 3: getGeoJson
-app.get('/geojson/:tableName/:id?', async (req, res) => {
-  const { tableName, id } = req.params;
-
-  try {
-    const sanitizedTableName = pgFormat.ident(tableName);
-    let query;
-    let params = [];
-
-    if (id) {
-      // Query untuk 1 fitur
-      query = `
-        SELECT jsonb_build_object(
-          'type', 'FeatureCollection',
-          'features', jsonb_agg(
-            jsonb_build_object(
-              'type', 'Feature',
-              'geometry', ST_AsGeoJSON(ST_Transform(t.geom, 4326), 15)::jsonb,
-              'properties', to_jsonb(t) - 'geom'
-            )
-          )
-        ) AS geojson
-        FROM public.${sanitizedTableName} t
-        WHERE t.id = $1;
-      `;
-      params.push(id);
-    } else {
-      // Query untuk semua fitur
-      query = `
-        SELECT jsonb_build_object(
-          'type', 'FeatureCollection',
-          'features', jsonb_agg(
-            jsonb_build_object(
-              'type', 'Feature',
-              'geometry', ST_AsGeoJSON(ST_Transform(t.geom, 4326), 15)::jsonb,
-              'properties', to_jsonb(t) - 'geom'
-            )
-          )
-        ) AS geojson
-        FROM public.${sanitizedTableName} t;
-      `;
-    }
-
-    const result = await pool.query(query, params);
-
-    if (!result.rows.length || !result.rows[0].geojson.features) {
-      return res.status(204).send(); // No Content jika tidak ada data
-    }
-
-    res.setHeader('Content-Type', 'application/json');
-    res.send(result.rows[0].geojson);
-  } catch (error) {
-    console.error('Error fetching GeoJSON data:', error.stack);
+    console.error('Error fetching search results:', error.stack);
     res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
